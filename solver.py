f18233e3f0335afd8a5f11ebab90437fdf144f78
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-10-16 16:48:02 +0300

    Move parsing to separate method, validate len of each line

--- solver.py
+++ solver.py
@@ -15,6 +15,38 @@ def line_to_tokens(line):
         tokens.append(t)
     return tokens
 
+def parse(f):
+    puzzle_size = None
+    puzzle = []
+    for line in f:
+        tokens = line_to_tokens(line)
+        if len(tokens) == 0:
+            continue
+        if puzzle_size is None:
+            if len(tokens) != 1:
+                raise ValueError(f"Bad size tokens: {tokens}")
+                exit(1)
+            try:
+                puzzle_size = int(tokens[0])
+            except ValueError:
+                raise ValueError(f"Bad token: {tokens[0]}")
+                exit(1)
+        else:
+            if len(tokens) != puzzle_size:
+                raise ValueError(f"Bad tokens len: {tokens}")
+                exit(1)
+            try:
+                puzzle_line = [int(tk) for tk in tokens]
+                if len(puzzle_line) != puzzle_size:
+                    raise ValueError("Bad puzzle formatting")
+                puzzle.append(puzzle_line)
+            except ValueError:
+                raise ValueError(f"Bad tokens: {tokens}")
+                exit(1)
+    if len(puzzle) != puzzle_size:
+        raise ValueError("Puzzle is too short")
+    return puzzle_size, puzzle
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('puzzle', help='path to puzzle file')
@@ -26,32 +58,11 @@ if __name__ == '__main__':
         print("No such file. Nice try, though.")
         exit(1)
     
-    puzzle_size = None
-    puzzle = []
-    for line in f:
-        tokens = line_to_tokens(line)
-        if len(tokens) == 0:
-            continue
-        if puzzle_size is None:
-            if len(tokens) != 1:
-                print(f"Bad size tokens: {tokens}")
-                exit(1)
-            try:
-                puzzle_size = int(tokens[0])
-            except ValueError:
-                print(f"Bad token: {tokens[0]}")
-                exit(1)
-        else:
-            if len(tokens) != puzzle_size:
-                print(f"Bad tokens len: {tokens}")
-                exit(1)
-            try:
-                puzzle_line = [int(tk) for tk in tokens]
-                puzzle.append(puzzle_line)
-            except ValueError:
-                print(f"Bad tokens: {tokens}")
-                exit(1)
-    if len(puzzle) != puzzle_size:
-        print("Puzzle is too short")
+    try:
+        puzzle_size, puzzle = parse(f)
+    except ValueError as e:
+        print(e)
         exit(1)
+    
+    
     print("PUZZLE PARSED:", puzzle)
